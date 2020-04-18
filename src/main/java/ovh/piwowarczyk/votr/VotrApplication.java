f0e614ff87886dbcd0e738fc8e662d38f0c553b8
jpiwowarczyk <jl.piwowarczyk@gmail.com>
2020-04-18 09:26:13 +0200

    Inicjalizacja projektu w repozytorium

---
+++ src/main/java/ovh/piwowarczyk/votr/VotrApplication.java
@@ -0,0 +1,13 @@
+package ovh.piwowarczyk.votr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ovh/piwowarczyk/votr/VotrApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.piwowarczyk.votr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
