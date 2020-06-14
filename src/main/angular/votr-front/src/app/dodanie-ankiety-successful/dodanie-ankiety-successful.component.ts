ae5cf1e9199d3df3916cb81bf9c46f610ad9464d
Angelika Okarmus <angelika.okarmus@student.up.krakow.pl>
2020-06-14 12:35:59 +0200

    Dodanie komponentu zawierającego komunikat o poprawnym utworzeniu ankiety.

---
+++ src/main/angular/votr-front/src/app/dodanie-ankiety-successful/dodanie-ankiety-successful.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dodanie-ankiety-successful',
+  templateUrl: './dodanie-ankiety-successful.component.html',
+  styleUrls: ['./dodanie-ankiety-successful.component.css']
+})
+export class DodanieAnkietySuccessfulComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
