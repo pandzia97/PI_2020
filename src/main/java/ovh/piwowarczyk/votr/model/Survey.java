6bf0aac3c11dec0c86588889bf5ed0074a3928ce
jpiwowarczyk <jl.piwowarczyk@gmail.com>
2020-09-06 17:41:20 +0200

    Aktualizacja modelu pod framework hibernate

--- src/main/java/ovh/piwowarczyk/votr/model/Survey.java
+++ src/main/java/ovh/piwowarczyk/votr/model/Survey.java
@@ -10,17 +10,12 @@ public class Survey {
     @GeneratedValue
     private long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User author;
-
     private String name;
 
     @OneToMany(mappedBy = "survey", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Question> questions;
 
     private Boolean active;
-
-    private Boolean finished;
 
 
     public Survey() {}
@@ -30,14 +25,6 @@ public class Survey {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
     }
 
     public String getName() {
@@ -62,13 +49,5 @@ public class Survey {
 
     public void setActive(Boolean active) {
         this.active = active;
-    }
-
-    public Boolean getFinished() {
-        return finished;
-    }
-
-    public void setFinished(Boolean finished) {
-        this.finished = finished;
     }
 }
