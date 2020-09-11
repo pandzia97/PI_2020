56071bc7b942ef7d84e01e0c4c8db7081ffd75c7
Angelika Okarmus <angelika.okarmus@student.up.krakow.pl>
2020-09-11 21:21:34 +0200

    Stworzenie formularza dodawania ankiety w oparciu o komponenty.

---
+++ src/main/angular/votr-front/src/app/option/option.ts
@@ -0,0 +1,11 @@
+export class Option {
+    id: number;
+    value: String;
+
+
+  constructor(id?: number, value?: String) {
+    this.id = id;
+    this.value = value ? value : "Podaj odpowiedź";
+  }
+}
+
