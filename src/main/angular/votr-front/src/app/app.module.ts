62f3c1c578762804bd6200650875803e73cc330a
Angelika Okarmus <angelika.okarmus@student.up.krakow.pl>
2020-09-11 21:23:14 +0200

    Zmiana mechanizmu działania aplikacji.

--- src/main/angular/votr-front/src/app/app.module.ts
+++ src/main/angular/votr-front/src/app/app.module.ts
@@ -6,12 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ListaAnkietComponent } from './lista-ankiet/lista-ankiet.component';
 import { RejestracjaComponent } from './rejestracja/rejestracja.component';
-import { DodanieAnkietyComponent } from './dodanie-ankiety/dodanie-ankiety.component';
+import { SurveyComponent } from './survey/survey.component';
 import { MenuComponent } from './menu/menu.component';
 import { HelloComponent } from './hello/hello.component';
 import { StopkaComponent } from './stopka/stopka.component';
 import { ErrorComponent } from './error/error.component';
 import { SuccessfulComponent } from './successful/successful.component';
+import { QuestionComponent } from './question/question.component';
+import { OptionComponent } from './option/option.component';
+import {FormsModule} from "@angular/forms";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
@@ -19,16 +26,23 @@ import { SuccessfulComponent } from './successful/successful.component';
     LoginComponent,
     ListaAnkietComponent,
     RejestracjaComponent,
-    DodanieAnkietyComponent,
+    SurveyComponent,
     MenuComponent,
     HelloComponent,
     StopkaComponent,
     ErrorComponent,
-    SuccessfulComponent
+    SuccessfulComponent,
+    QuestionComponent,
+    OptionComponent,
+    AboutComponent,
+    MainComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
