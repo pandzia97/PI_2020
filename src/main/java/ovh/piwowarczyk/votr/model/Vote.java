56c01223827def9327bd82192c16aef37c920671
jpiwowarczyk <jl.piwowarczyk@gmail.com>
2020-09-11 09:48:35 +0200

    Dodanie encji do zapisu odpowiedzi w BD

---
+++ src/main/java/ovh/piwowarczyk/votr/model/Vote.java
@@ -0,0 +1,61 @@
+package ovh.piwowarczyk.votr.model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long voteID;
+    private String hashedIdentifier;
+    @ManyToOne
+    private Survey survey;
+    private Date date;
+    @OneToMany(mappedBy = "vote")
+    private List<Answer> answers;
+
+    public Vote() {}
+
+    public Long getVoteID() {
+        return voteID;
+    }
+
+    public void setVoteID(Long voteID) {
+        this.voteID = voteID;
+    }
+
+    public String getHashedIdentifier() {
+        return hashedIdentifier;
+    }
+
+    public void setHashedIdentifier(String hashedIdentifier) {
+        this.hashedIdentifier = hashedIdentifier;
+    }
+
+    public Survey getSurvey() {
+        return survey;
+    }
+
+    public void setSurvey(Survey survey) {
+        this.survey = survey;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<Answer> answers) {
+        this.answers = answers;
+    }
+}
