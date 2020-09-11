c1b343ec0bd241f86373f60aa350e7faaa0220d7
jpiwowarczyk <jl.piwowarczyk@gmail.com>
2020-09-11 19:21:07 +0200

    Zmiana implementacji klas UserDAO i SurveyDAO jako rozzszerzenia klasy HibernateDAO

--- src/main/java/ovh/piwowarczyk/votr/dao/UserDAO.java
+++ src/main/java/ovh/piwowarczyk/votr/dao/UserDAO.java
@@ -1,30 +1,9 @@
 package ovh.piwowarczyk.votr.dao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import ovh.piwowarczyk.votr.model.User;
 
-public class UserDAO {
-    private JdbcTemplate jdbcTemplate;
-
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public int createUser(User user){
-        String query = "";
-
-        return jdbcTemplate.update(query);
-    }
-
-    public int updateUser(User user){
-        String query = "";
-
-        return jdbcTemplate.update(query);
-    }
-
-    public int deleteUser(User user){
-        String query = "";
-
-        return jdbcTemplate.update(query);
+public class UserDAO extends HibernateDAO {
+    public UserDAO(){
+        setClazz(User.class);
     }
 }
