50a38f764a5fcec9c75d2e4612022941a922a7a5
Angelika Okarmus <angelika.okarmus@student.up.krakow.pl>
2020-09-11 21:23:14 +0200

    Zmiana mechanizmu działania aplikacji.

--- src/main/angular/votr-front/src/app/app-routing.module.ts
+++ src/main/angular/votr-front/src/app/app-routing.module.ts
@@ -3,11 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {HelloComponent} from "./hello/hello.component";
 import {RejestracjaComponent} from "./rejestracja/rejestracja.component";
+import {SurveyComponent} from "./survey/survey.component";
+import {ErrorComponent} from "./error/error.component";
+import {SuccessfulComponent} from "./successful/successful.component";
+import {AboutComponent} from "./about/about.component";
 
 const routes: Routes = [
   { path: 'login-component', component: LoginComponent },
+  { path: 'dodanie-ankiety', component: SurveyComponent},
   { path: '', component: HelloComponent},
-  { path: 'register-component', component: RejestracjaComponent}
+  { path: 'register-component', component: RejestracjaComponent},
+  { path: 'successful', component: SuccessfulComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'ankieta', component: SurveyComponent},
+  { path: '**', component: ErrorComponent}
+
+
 
   //{ path: '**', component: //errorPage}
 ];
