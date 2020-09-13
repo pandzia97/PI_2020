d3938c6f24e63ab4fed12d599081d259d2aeb3fd
jpiwowarczyk <jl.piwowarczyk@gmail.com>
2020-09-13 19:20:04 +0200

    Dodanie pobierania pojedynczej ankiety przez REST API

--- src/main/java/ovh/piwowarczyk/votr/security/ApplicationSecurityConfig.java
+++ src/main/java/ovh/piwowarczyk/votr/security/ApplicationSecurityConfig.java
@@ -12,7 +12,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().disable()
+                .csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
                 .permitAll();
