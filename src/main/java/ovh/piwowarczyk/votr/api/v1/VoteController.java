2b916ddf1d069e8a3d2dda7d1c285afbfdce96d4
jpiwowarczyk <jl.piwowarczyk@gmail.com>
2020-09-16 22:40:55 +0200

    zmiany w kontrolerze VoteController

--- src/main/java/ovh/piwowarczyk/votr/api/v1/VoteController.java
+++ src/main/java/ovh/piwowarczyk/votr/api/v1/VoteController.java
@@ -1,30 +1,36 @@
 package ovh.piwowarczyk.votr.api.v1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ovh.piwowarczyk.votr.model.Survey;
 import ovh.piwowarczyk.votr.model.Vote;
 import ovh.piwowarczyk.votr.services.VoteService;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("api/v1/votes")
 public class VoteController {
+    Logger logger = LoggerFactory.getLogger(VoteController.class);
+
 
     @Autowired
     private VoteService voteService;
 
-    @GetMapping("survey/{id}")
+    @GetMapping("survey/{surveyId}")
     @CrossOrigin
-    public List<Vote> getVotesForSurvey(Long surveyId){
+    public List<Vote> getVotesForSurvey(@PathVariable Long surveyId){
         return voteService.getVotesForSurveyId(surveyId);
     }
 
     @PostMapping
     @CrossOrigin
     public String addVote(@RequestBody Vote vote){
+        logger.debug("received Answer list size: " + vote.getAnswers().size());
         Vote addedVote = voteService.createNew(vote);
+        logger.debug("saved Answer list size: " + addedVote.getAnswers().size());
         return "{\"HASHED_IDENTIFIER\": \"" + addedVote.getHashedIdentifier() + "\"}";
     }
 
